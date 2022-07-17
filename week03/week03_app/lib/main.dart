575f5451ade83f384058ff169629f93fe5acd38e
ssb0498 <newssb1@naver.com>
2022-07-17 21:43:19 +0900

    week03 sun end

--- week03/week03_app/lib/main.dart
+++ week03/week03_app/lib/main.dart
@@ -1,7 +1,35 @@
-
 import 'package:flutter/material.dart';
+import 'package:week03_app/screens/record.dart';
+import 'package:week03_app/screens/rlist.dart';
 
-void main()
-{
-  runApp(app)
-};
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<bool> _isSelected = [
+    true,
+    false,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'week03',
+      initialRoute: '/0',
+      routes: {
+        '/0': (context) => Record(
+              isSelected: _isSelected,
+            ),
+        '/1': (context) => RList(isSelected: _isSelected),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
